eee72519aa83ccc94c052966cb29ccc06aff6827
nagipragalathan <kk2069037@gmail.com>
2023-05-05 12:40:45 +0530

    Ai Tranined are Updated

--- Dragster/Routes/AI_Functions.py
+++ Dragster/Routes/AI_Functions.py
@@ -2,6 +2,35 @@ from .Tool.Tools import get_stackoverflow_link, get_example_code_gfg, get_answer
 from django.shortcuts import render
 from django.http import JsonResponse
 import wikipedia
+from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_content(query):
+    # Search Google for web pages related to the query
+    urls = list(search(query, num_results=3))
+    print("Web Scrap started")
+    # Scrape the contents of each web page
+    content = []
+    for url in urls:
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            # Extract the text content from the page
+            text = soup.get_text()
+            content.append(text)
+        except:
+            # Skip any pages that cannot be accessed
+            pass
+    try:
+        # return content
+        return "".join(content[0].split(".")[0:3])
+    except:
+        try:
+            return content
+        except:
+            return "I can't understand please try any differnt question or give me more detials about your question"
 
 def Code_scriping(request):
     context = {}
@@ -44,12 +73,15 @@ def chatbot_res(request):
         if code:
             response = code
         else:
-            wikipedia.set_lang("en")
-            # Get the summary of a page
-            page = wikipedia.page(message)
-            summary = page.summary
-            response = summary
-        return JsonResponse({"response": response})
+            try:
+                wikipedia.set_lang("en")
+                # Get the summary of a page
+                page = wikipedia.page(message)
+                summary = page.summary
+                response = summary
+                return JsonResponse({"response": response})
+            except:
+                return JsonResponse({"response":get_content(message)})
     else:
         return JsonResponse({"error": "Invalid request method"})
 
